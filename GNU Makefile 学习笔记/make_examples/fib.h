1c4ad752a975b3e83573cb5981cb9f4e5eaeaf4d
jamesamcones <a587212@126.com>
2022-10-27 10:46:51 +0800

    vault backup: 2022-10-27 10:46:51
    Affected files:
    .DS_Store
    .obsidian/workspace.json
    GNU Makefile 学习笔记/make_examples/fib.h
    GNU Makefile 学习笔记/make_examples/main.cpp

---
+++ GNU Makefile 学习笔记/make_examples/fib.h
@@ -0,0 +1,5 @@
+long long fib(int n)
+{
+  if(n==1||n==2)return 1;
+  return fib(n-1)+fib(n-2);
+}
---
+++ GNU Makefile 学习笔记/make_examples/main.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+#include "fib.h"
+using namespace std;
+int main()
+{
+  cout<<fib(10)<<endl;
+  return 0;
+}
